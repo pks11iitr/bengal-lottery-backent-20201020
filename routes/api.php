96cfac55fb0453d30d5b0f5da57a669eb28f813b
Alam <badre.alam1993@gmail.com>
2020-12-08 18:07:23 +0530

    commission calculation

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,8 @@ $api->group(['middleware' => ['isactiveuser']], function ($api) {
     $api->get('notifications', ['as' => 'notifications.list', 'uses' => 'Portal\Api\NotificationController@index']);
 });
 
+$api->get('downline-history-game', ['as' => 'api.downline', 'uses' => 'Portal\Api\DownlinegamehistoryController@index']);
+
 $api->get('history-game', ['as' => 'api.history.game', 'uses' => 'Portal\Api\BookingHistoryController@historygame']);
 $api->post('game-book', ['as' => 'api.gamebooking', 'uses' => 'Portal\Api\GameController@gamebooking']);
 
