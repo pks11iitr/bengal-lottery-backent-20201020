12c3cd7d95ec79233ee192b75634758433b60b2b
Alam <badre.alam1993@gmail.com>
2020-12-08 11:39:57 +0530

    api work

--- app/Http/Controllers/Portal/DashboardController.php
+++ app/Http/Controllers/Portal/DashboardController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Portal;
 use App\Models\CompanyProducts;
 use App\Models\Game;
 use App\Models\MappedCode;
+use App\Models\UserStat;
 use App\QrCodes;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use DB;
 class DashboardController extends Controller
 {
     public function dashboard(Request $request){
@@ -26,6 +27,18 @@ class DashboardController extends Controller
         $games=Game::orderBy('id','DESC')
             ->get();
         $totalgames=$games->count();
+
+        $total=0;
+        $agents=User::where('parent_id',$user->id)->get();
+        foreach($agents as $agent){
+            $commision=UserStat::where('user_id',$agent->id) ;
+           $agenttotalbid= $commision->sum('digit0') + $commision->sum('digit1') +$commision->sum('digit2') +$commision->sum('digit3') +$commision->sum('digit4') + $commision->sum('digit5')+ $commision->sum('digit6')+$commision->sum('digit7') + $commision->sum('digit8') + $commision->sum('digit9');
+        $total=   $total+ ($agenttotalbid*($agent->rate-$user->rate));
+        }
+         return  $total;
+
+
+
       //  $qrcode=QrCodes::where('user_id', $user->id)->sum('total');
 //        $mappedqr=MappedCode::where('user_id', $user->id)->sum('total');
 //        $skus=CompanyProducts::where('user_id', $user->id)
